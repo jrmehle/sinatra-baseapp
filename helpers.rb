00399ab2b11281fa71499178ce467db618697d6e
Jared Mehle <jrmehle@gmail.com>
2009-10-27 09:45:46 -0500

    Added the HTML sanitizer from Rack as an alias and fixed a small bug in the image tag helper.

--- helpers.rb
+++ helpers.rb
@@ -1,6 +1,10 @@
 module Helpers
+  include Rack::Utils
+  alias_method :h, :escape_html
+  
+  
   def image_tag(path_to_file, options = {})
-    path_to_file = File.join 'images', path_to_file
+    path_to_file = File.join '/images', path_to_file
     "<img src=\"#{path_to_file}\" width=\"#{options[:width]}\" height=\"#{options[:height]}\" alt=\"#{options[:alt]}\" #{'id="' + options[:id] + '"' if options[:id]} #{'class="' + options[:class] + '"' if options[:class]} #{'title="' + options[:title] + '"' if options[:title]} />"
   end
   
@@ -10,7 +14,7 @@ module Helpers
   
   def link_to_unless_current(text, path = "#")
     if current_page? path
-      "<span id=\"current\" title=\"#{text} is the current page\">#{text}</span>"
+      "<span id=\"current\">#{text}</span>"
     else
       "<a href=\"#{path}\">#{text}</a>"
     end
@@ -42,4 +46,13 @@ module Helpers
     
     "<a href=\"#{string}\" class=\"email #{options[:class] if options.include?(:class)}\">#{text || email_address_encoded}</a>"
   end
+
+  # uncomment and add the routes and stylesheets needed
+  # def stylesheet_for_current_page
+  #   if ['/', '/services', '/projects', '/process', '/contact'].include? request.path_info
+  #     stylesheet = request.path_info.gsub('/', '')
+  #     stylesheet = 'index' if request.path_info == '/'
+  #     return "<link rel=\"stylesheet\" href=\"/stylesheets/#{stylesheet}.css\" type=\"text/css\" media=\"screen\" charset=\"utf-8\">"
+  #   end
+  # end
 end
