1baa2994a09938a8244f103b8df97e3b639bee10
Jared Mehle <jrmehle@gmail.com>
2011-01-07 22:37:25 -0600

    Update style

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ get '/' do
 
   <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 
-  <h2>Header Level 2</h2>
+  <h2>Header Level 2 - Ordered List</h2>
 
   <ol>
      <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
@@ -23,7 +23,7 @@ get '/' do
 
   <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
 
-  <h3>Header Level 3</h3>
+  <h3>Header Level 3 - Unordered List</h3>
 
   <ul>
      <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
